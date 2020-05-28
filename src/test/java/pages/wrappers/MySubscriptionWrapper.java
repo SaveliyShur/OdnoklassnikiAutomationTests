86942a327e378de96cdb93b8cda2484ea42e96e1
Saveliy <cavacava1999@mail.ru>
2020-05-28 14:22:42 +0300

    Commit тест с добавлением работает

--- src/test/java/pages/wrappers/MySubscriptionWrapper.java
+++ src/test/java/pages/wrappers/MySubscriptionWrapper.java
@@ -11,8 +11,9 @@ public class MySubscriptionWrapper extends BaseWrapper {
     private static final By NAME_LOCATOR = By.xpath(".//*[@class='n-t bold']");
     private static final By AVATAR = By.xpath(".//*[@class='user-grid-card_avatar']");
     private static final By LAYER_MOVE_TO_AVATAR = By.xpath("//*[@class='shortcutRoundedPanel']");
-    private static final By UNSUBSCRIBE = By.xpath(".//*[contains(@class , 'ic_subscribe-off')]");
-
+    private static final By UNSUBSCRIBE = By.xpath(".//*[@class='gwt-shortcutMenu-iconlink-item']/span[text()='Отписаться']");
+    private static final By UNSUBSCRIBE_FINAL_LAYER = By.xpath("//*[@id='popLayerBodyWrapper']");
+    private static final By UNSUBSCRIBE_FINAL_BUTTON = By.xpath("//*[@value='Отписаться']");
     public MySubscriptionWrapper(WebElement icon, WebDriver driver) {
         super(icon, driver);
     }
@@ -27,6 +28,9 @@ public class MySubscriptionWrapper extends BaseWrapper {
         actions.moveToElement(avatar).build().perform();
         assertLocator(driver,LAYER_MOVE_TO_AVATAR);
         icon.findElement(LAYER_MOVE_TO_AVATAR).findElement(UNSUBSCRIBE).click();
+        assertLocator(driver,UNSUBSCRIBE_FINAL_LAYER);
+        driver.findElement(UNSUBSCRIBE_FINAL_LAYER).findElement(UNSUBSCRIBE_FINAL_BUTTON).click();
+
     }
 
 }
